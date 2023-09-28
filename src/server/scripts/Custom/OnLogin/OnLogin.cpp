928449de97cd4807ed6023b83022ceddf288ff14
fischerlol <fischer-lochlan@iinet.net.au>
2023-09-29 03:23:21 +1000

    Update OnLogin.cpp

--- src/server/scripts/Custom/OnLogin/OnLogin.cpp
+++ src/server/scripts/Custom/OnLogin/OnLogin.cpp
@@ -3,52 +3,166 @@
 #include "Pet.h"
 #include "ObjectMgr.h"
 #include "Log.h"
+#include "Item.h"
+#include "Bag.h"
+#include "WorldSession.h"
 
 class OnLoginPlayerScripts : public PlayerScript
 {
 public:
     OnLoginPlayerScripts() : PlayerScript("OnLoginPlayerScripts") {}
 
-    void OnLogin(Player* player, bool loginFirst) override
+    void HandleUndead(Player* player)
     {
-        int STARTER_QUESTS[33] = { 12593, 12619, 12842, 12848, 12636, 12641, 12657, 12678, 12679, 12680, 12687, 12698, 12701, 12706, 12716, 12719, 12720, 12722, 12724, 12725, 12727, 12733, -1, 12751,
-        12754, 12755, 12756, 12757, 12779, 12801, 13165, 13166 };
-
-        if (loginFirst) // This is the player's first login
+        if (player->getRace() != RACE_UNDEAD_PLAYER)
         {
-            switch (player->getClass())
-            {
-            case CLASS_DEATH_KNIGHT:
-                for (int questId : STARTER_QUESTS)
-                {
-                    Quest const* questTemplate = sObjectMgr->GetQuestTemplate(questId);
-                    if (questTemplate == nullptr)
-                    {
-                        TC_LOG_INFO("server.worldserver", "Quest %u not found", questId);
-                        continue;
-                    }
+            return;
+        }
 
-                    if (player->GetQuestStatus(questId) == QUEST_STATUS_NONE)
+        player->RemoveAura(73523);
+    }
+
+    void HandleGnome(Player* player)
+    {
+        if (player->getRace() != RACE_GNOME)
+        {
+            return;
+        }
+
+        player->RemoveAura(80653);
+    }
+
+    void HandleDKSkip(Player* player)
+    {
+        if (player->getClass() != CLASS_DEATH_KNIGHT)
+        {
+            return;
+        }
+
+        player->LearnSpell(50977, false);
+        ClearBags(player);
+        ClearGear(player);
+        player->SaveToDB();
+    }
+
+    void HandleWorgenSkip(Player* player)
+    {
+        if (player->getRace() != RACE_WORGEN)
+        {
+            return;
+        }
+
+        int WORGEN_QUEST = 26706;
+        Quest const* questTemplate = sObjectMgr->GetQuestTemplate(WORGEN_QUEST);
+
+        if (player->GetQuestStatus(WORGEN_QUEST) == QUEST_STATUS_NONE)
+        {
+            player->AddQuest(questTemplate, nullptr);
+            player->RewardQuest(questTemplate, 0, player, false);
+        }
+
+        ClearBags(player);
+        ClearGear(player);
+        player->SaveToDB();
+    }
+
+    void HandleGoblinSkip(Player* player)
+    {
+        if (player->getRace() != RACE_GOBLIN)
+        {
+            return;
+        }
+
+        int GOBLIN_QUEST = 25265;
+        Quest const* questTemplate = sObjectMgr->GetQuestTemplate(GOBLIN_QUEST);
+
+        if (player->GetQuestStatus(GOBLIN_QUEST) == QUEST_STATUS_NONE)
+        {
+            player->AddQuest(questTemplate, nullptr);
+            player->RewardQuest(questTemplate, 0, player, false);
+        }
+
+        ClearBags(player);
+        ClearGear(player);
+        player->SaveToDB();
+    }
+
+    void ClearBags(Player* player)
+    {
+        // First, remove all items from the bags.
+        for (uint8 bag = INVENTORY_SLOT_BAG_START; bag < INVENTORY_SLOT_BAG_END; ++bag)
+        {
+            Bag* equippedBag = player->GetBagByPos(bag);
+            if (equippedBag)
+            {
+                for (uint32 slot = 0; slot < equippedBag->GetBagSize(); ++slot)
+                {
+                    Item* item = player->GetItemByPos(bag, slot);
+                    if (item)
                     {
-                        player->AddQuest(questTemplate, nullptr);
-                        player->RewardQuest(questTemplate, 0, player, false);
+                        // Skip Hearthstone
+                        if (item->GetEntry() == 6948)
+                            continue;
+
+                        // Destroy the item
+                        player->DestroyItem(bag, slot, true);
                     }
                 }
 
-                break;
+                // Now that the bag is empty, we can destroy it.
+                player->DestroyItem(INVENTORY_SLOT_BAG_0, bag, true);
             }
-
-
-            float x = 4331.907715f; // Replace with the X coordinate of the destination
-            float y = -2881.159424f; // Replace with the Y coordinate of the destination
-            float z = 0.921211f; // Replace with the Z coordinate of the destination
-            float o = 2.265909f; // Replace with the orientation of the destination
-            uint32 mapId = 0; // Replace with the map ID of the destination
-            //X: 4331.907715 Y: -2881.159424 Z: 0.921311 Orientation: 2.281618
-            player->TeleportTo(mapId, x, y, z, o);
         }
 
-        
+        // Then, remove all items from the main backpack.
+        for (uint32 slot = 23; slot <= 38; ++slot)
+        {
+            Item* item = player->GetItemByPos(INVENTORY_SLOT_BAG_0, slot);
+            if (item)
+            {
+                // Skip Hearthstone
+                if (item->GetEntry() == 6948)
+                    continue;
+
+                // Destroy the item
+                player->DestroyItem(INVENTORY_SLOT_BAG_0, slot, true);
+            }
+        }
+
+        player->SetMoney(0);
+    }
+
+    void ClearGear(Player* player)
+    {
+        for (uint8 i = EQUIPMENT_SLOT_START; i < EQUIPMENT_SLOT_END; ++i)
+        {
+            if (Item* haveItemEquipped = player->GetItemByPos(INVENTORY_SLOT_BAG_0, i))
+            {
+                if (haveItemEquipped)
+                {
+                    player->DestroyItem(INVENTORY_SLOT_BAG_0, i, true);
+                }
+            }
+        }
+    }
+
+    void OnLogin(Player* player, bool loginFirst) override
+    {
+        if (loginFirst) // This is the player's first login
+        {
+            HandleDKSkip(player);
+            HandleWorgenSkip(player);
+            HandleGoblinSkip(player);
+            HandleUndead(player);
+            HandleGnome(player);
+
+            float x = 4324.137695f; // Replace with the X coordinate of the destination
+            float y = -2871.365234f; // Replace with the Y coordinate of the destination
+            float z = 2.549443f; // Replace with the Z coordinate of the destination
+            float o = 2.254905f; // Replace with the orientation of the destination
+            uint32 mapId = 0; // Replace with the map ID of the destination
+            player->TeleportTo(mapId, x, y, z, o);
+        }
     }
 };
 
@@ -59,14 +173,11 @@ public:
 
     void OnLogin(Player* player, bool /*firstLogin*/) override
     {
-        TC_LOG_INFO("server.worldserver", "Player %s has logged in", player->GetName().c_str());
-
         Pet* pet = player->GetPet();
 
         if (pet)
         {
             pet->SetObjectScale(1.0f); // Set the pet's scale to 1.0
-            TC_LOG_INFO("server.worldserver", "Set scale of pet for player %s to 1.0", player->GetName().c_str());
         }
     }
 };
